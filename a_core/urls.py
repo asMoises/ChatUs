51e3860b2a209d5173f47784a53270558b3de47d
Moisés Silva <moiss.silva@outlook.com>
2025-04-19 22:43:13 -0300

    Renderizando a página de profile.

--- a_core/urls.py
+++ a_core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from a_home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
+    path('profile/', include('a_users.urls')),
 ]
