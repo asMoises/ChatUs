1f088b01f63e90699085de291ab54afe45b3f5f8
Moisés Silva <moiss.silva@outlook.com>
2025-04-19 23:44:19 -0300

    Criação da função de atualizar profile

---
+++ a_users/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from django import forms
+from .models import Profile
+
+class ProfileForm(ModelForm):
+  class Meta:
+    model = Profile
+    exclude = ['user']
+    widgets = {
+      'image': forms.FileInput(), 
+      'displayname': forms.TextInput(attrs={'placeholder': 'Add displayname'}),
+      'info': forms.Textarea(attrs={'rows':3,'placeholder':'Add Information'}),     
+    }
