fd9ec76c0b34e4d79c149d72c7e2eff44408a962
Moisés Silva <moiss.silva@outlook.com>
2025-04-20 12:49:02 -0300

    Recurso de logout e login ok

--- a_core/settings.py
+++ a_core/settings.py
@@ -138,3 +138,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_AUTHENTICATOR_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+
